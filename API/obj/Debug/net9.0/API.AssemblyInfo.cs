e9903f20760d05cc025c493f1880164f9531b4f3
Tymoteusz Broda <tymoteusz.91100@gmail.com>
2024-12-05 23:46:51 +0100

    added .gitignore

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5cb92aa6532ef26259f90f19d76cd649f5f5103")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c45b9395d5e198e22188e9c385b9bb97eeb77e73")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
