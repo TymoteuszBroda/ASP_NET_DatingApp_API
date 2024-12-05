fc33c89350f9df2bc531c412fc7f3f028b510699
Tymoteusz Broda <tymoteusz.91100@gmail.com>
2024-12-05 22:24:43 +0100

    created database with example values

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b59a9994e60f4944650587ec3ce643dfe3322c54")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f251377329763158302c86d4e09e9bfc010752f")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
