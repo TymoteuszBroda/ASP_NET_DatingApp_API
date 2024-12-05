bf5c315d80b7fb87a290940ec830d35237dc4b54
Tymoteusz Broda <tymoteusz.91100@gmail.com>
2024-12-05 21:49:26 +0100

    added EntityFramework sqlite and design nuget packages

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+842b8fdf51dc2f34907040055121c096baaff876")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a1eef7ec1afa2369edec995b5513a5f2a4e7d17")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
