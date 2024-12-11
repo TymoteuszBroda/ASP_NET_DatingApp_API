c020895af24d57f99b839d397fe20fd10d769fe6
Tymoteusz Broda <tymoteusz.91100@gmail.com>
2024-12-11 19:34:42 +0100

    added dropdown menu

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -2,10 +2,11 @@ import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
 import { NgIf } from '@angular/common';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 @Component({
   selector: 'app-nav',
-  imports: [FormsModule, NgIf],
+  imports: [FormsModule, BsDropdownModule],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css',
 })
