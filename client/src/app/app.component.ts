21958fdf93e85ddfafdf2dd7bf965e14f042812a
Tymoteusz Broda <tymoteusz.91100@gmail.com>
2024-12-11 21:12:43 +0100

    added persisting user login

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Observable } from 'rxjs';
 import { NavComponent } from './nav/nav.component';
+import { AccountService } from './_services/account.service';
 
 @Component({
   selector: 'app-root',
@@ -13,10 +14,27 @@ import { NavComponent } from './nav/nav.component';
 export class AppComponent implements OnInit {
   http = inject(HttpClient);
   title = 'DatingApp';
+  private accountService = inject(AccountService);
   users: any;
   url: string = 'https://localhost:5001';
 
   ngOnInit(): void {
+    this.getUsers();
+    this.setCurrentUser();
+  }
+
+  setCurrentUser()
+  {
+    const userString = localStorage.getItem('user');
+    if(!userString)
+    {
+      return;
+    }
+    const user = JSON.parse(userString);
+    this.accountService.currentUser.set(user);
+  }
+
+  getUsers() {
     this.http.get(`${this.url}/api/users`).subscribe({
       next: (response) => (this.users = response),
       error: (error) => console.log(error),
