5f8e74abf2f62cd91795e53a460c7c54bb1b8afe
Tymoteusz Broda <tymoteusz.91100@gmail.com>
2024-12-11 22:10:04 +0100

    added parent to child communication

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,9 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Observable } from 'rxjs';
+
 import { NavComponent } from './nav/nav.component';
 import { AccountService } from './_services/account.service';
-import { HomeComponent } from "./home/home.component";
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
@@ -13,33 +12,18 @@ import { HomeComponent } from "./home/home.component";
   styleUrl: './app.component.css',
 })
 export class AppComponent implements OnInit {
-  http = inject(HttpClient);
-  title = 'DatingApp';
   private accountService = inject(AccountService);
-  users: any;
-  url: string = 'https://localhost:5001';
 
   ngOnInit(): void {
-    this.getUsers();
     this.setCurrentUser();
   }
 
-  setCurrentUser()
-  {
+  setCurrentUser() {
     const userString = localStorage.getItem('user');
-    if(!userString)
-    {
+    if (!userString) {
       return;
     }
     const user = JSON.parse(userString);
     this.accountService.currentUser.set(user);
-  }
-
-  getUsers() {
-    this.http.get(`${this.url}/api/users`).subscribe({
-      next: (response) => (this.users = response),
-      error: (error) => console.log(error),
-      complete: () => console.log('request completed'),
-    });
   }
 }
--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,17 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RegisterComponent } from "../register/register.component";
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
   imports: [RegisterComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
   registerMode = false;
+  users: any;
+  http = inject(HttpClient);
+  url: string = 'https://localhost:5001';
 
-  registerToggle()
-  {
-    this.registerMode = !this.registerMode
+  ngOnInit(): void {
+    this.getUsers();
+    
+  }
+
+  registerToggle() {
+    this.registerMode = !this.registerMode;
+  }
+
+  getUsers() {
+    this.http.get(`${this.url}/api/users`).subscribe({
+      next: (response) => (this.users = response),
+      error: (error) => console.log(error),
+      complete: () => console.log('request completed'),
+    });
   }
 }
