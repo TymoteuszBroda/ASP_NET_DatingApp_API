a1b5cf151ff19f376f0e860761ac2989d3874b42
Tymoteusz Broda <tymoteusz.91100@gmail.com>
2024-12-20 20:53:12 +0100

    added hover effects for cards

--- client/src/app/members/member-card/member-card.component.ts
+++ client/src/app/members/member-card/member-card.component.ts
@@ -1,11 +1,11 @@
-import { Component, input } from '@angular/core';
+import { Component, input, ViewEncapsulation } from '@angular/core';
 import { Member } from '../../_models/Member';
 
 @Component({
   selector: 'app-member-card',
   imports: [],
   templateUrl: './member-card.component.html',
-  styleUrl: './member-card.component.css'
+  styleUrl: './member-card.component.css',
 })
 export class MemberCardComponent {
   member = input.required<Member>();
