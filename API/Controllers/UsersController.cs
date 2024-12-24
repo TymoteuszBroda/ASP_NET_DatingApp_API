ba11bb55558038933e9dbe17784ccc0566002106
Tymoteusz Broda <tymoteusz.91100@gmail.com>
2024-12-24 02:56:20 +0100

    Persisting changes in API

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Security.Claims;
 using API.Data;
 using API.DTOs;
 using API.Entities;
@@ -30,5 +31,30 @@ public class UsersController(IUserRepository userRepository, IMapper mapper) : B
             return NotFound();
         }
         return user;
+    }
+
+    [HttpPut]
+    public async Task<ActionResult> UpdateUser(MemberUpdateDTO memberUpdateDTO)
+    {
+        var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+        if (username == null)
+        {
+            return BadRequest("No username found in token");
+        }
+        var user = await userRepository.GetUserByUsermameAsync(username);
+
+        if (user == null)
+        {
+            return BadRequest("Could not find user");
+        }
+
+        mapper.Map(memberUpdateDTO, user);
+
+        if (await userRepository.SaveAllSync()) 
+        {
+            return NoContent();
+        }
+        return BadRequest("Failed to update the user");
     }
 }
