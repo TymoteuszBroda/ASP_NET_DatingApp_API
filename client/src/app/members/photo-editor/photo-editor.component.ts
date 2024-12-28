525769618367dc6c0d0f07904b3113715fc19094
Tymoteusz Broda <tymoteusz.91100@gmail.com>
2024-12-28 01:41:46 +0100

    added photo editor configuration

--- client/src/app/members/photo-editor/photo-editor.component.ts
+++ client/src/app/members/photo-editor/photo-editor.component.ts
@@ -1,12 +1,54 @@
-import { Component, input } from '@angular/core';
+import { Component, inject, input, OnInit, output } from '@angular/core';
 import { Member } from '../../_models/Member';
+import { NgClass, NgFor, NgIf, NgStyle } from '@angular/common';
+import { FileUploader, FileUploadModule } from 'ng2-file-upload';
+import { AccountService } from '../../_services/account.service';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-photo-editor',
-  imports: [],
+  imports: [NgIf, NgFor, NgStyle, NgClass, FileUploadModule],
   templateUrl: './photo-editor.component.html',
   styleUrl: './photo-editor.component.css'
 })
-export class PhotoEditorComponent {
+export class PhotoEditorComponent implements OnInit{
+private accountService = inject(AccountService)
+
   member = input.required<Member>();
+
+  uploader?: FileUploader;
+  hasBaseDropzoneOver = false;
+  baseUrl = environment.apiUrl;
+  memberChange = output<Member>();
+
+  ngOnInit(): void {
+    this.initializeUploader();
+  }
+
+  fileOverBase(e: any)
+  {
+    this.hasBaseDropzoneOver = e;
+  }
+  initializeUploader()
+  {
+    this.uploader = new FileUploader({
+      url: this.baseUrl+'users/add-photo',
+      authToken: 'Bearer '+this.accountService.currentUser()?.token,
+      isHTML5: true,
+      allowedFileType: ['image'],
+      removeAfterUpload: true,
+      autoUpload: false,
+      maxFileSize: 10*1024*1024
+    });
+    this.uploader.onAfterAddingFile = (file) =>{
+      file.withCredentials = false;
+    }
+    this.uploader.onSuccessItem =(item, response, status, headers) =>
+    {
+      const photo = JSON.parse(response);
+      const updatedMember = {...this.member()}
+      updatedMember.photos.push(photo);
+      this.memberChange.emit(updatedMember);
+    }
+  }
 }
