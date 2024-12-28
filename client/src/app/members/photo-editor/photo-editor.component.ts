7c88db1b7af9dc22422873ca1768652efd18029e
Tymoteusz Broda <tymoteusz.91100@gmail.com>
2024-12-28 01:55:41 +0100

    added ability for user to upload image via dropzone on page

--- client/src/app/members/photo-editor/photo-editor.component.ts
+++ client/src/app/members/photo-editor/photo-editor.component.ts
@@ -1,23 +1,23 @@
 import { Component, inject, input, OnInit, output } from '@angular/core';
 import { Member } from '../../_models/Member';
-import { NgClass, NgFor, NgIf, NgStyle } from '@angular/common';
+import { DecimalPipe, NgClass, NgFor, NgIf, NgStyle } from '@angular/common';
 import { FileUploader, FileUploadModule } from 'ng2-file-upload';
 import { AccountService } from '../../_services/account.service';
 import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-photo-editor',
-  imports: [NgIf, NgFor, NgStyle, NgClass, FileUploadModule],
+  imports: [NgIf, NgFor, NgStyle, NgClass, FileUploadModule, DecimalPipe],
   templateUrl: './photo-editor.component.html',
-  styleUrl: './photo-editor.component.css'
+  styleUrl: './photo-editor.component.css',
 })
-export class PhotoEditorComponent implements OnInit{
-private accountService = inject(AccountService)
+export class PhotoEditorComponent implements OnInit {
+  private accountService = inject(AccountService);
 
   member = input.required<Member>();
 
   uploader?: FileUploader;
-  hasBaseDropzoneOver = false;
+  hasBaseDropZoneOver = false;
   baseUrl = environment.apiUrl;
   memberChange = output<Member>();
 
@@ -25,30 +25,27 @@ private accountService = inject(AccountService)
     this.initializeUploader();
   }
 
-  fileOverBase(e: any)
-  {
-    this.hasBaseDropzoneOver = e;
+  fileOverBase(e: any) {
+    this.hasBaseDropZoneOver = e;
   }
-  initializeUploader()
-  {
+  initializeUploader() {
     this.uploader = new FileUploader({
-      url: this.baseUrl+'users/add-photo',
-      authToken: 'Bearer '+this.accountService.currentUser()?.token,
+      url: this.baseUrl + 'users/add-photo',
+      authToken: 'Bearer ' + this.accountService.currentUser()?.token,
       isHTML5: true,
       allowedFileType: ['image'],
       removeAfterUpload: true,
       autoUpload: false,
-      maxFileSize: 10*1024*1024
+      maxFileSize: 10 * 1024 * 1024,
     });
-    this.uploader.onAfterAddingFile = (file) =>{
+    this.uploader.onAfterAddingFile = (file) => {
       file.withCredentials = false;
-    }
-    this.uploader.onSuccessItem =(item, response, status, headers) =>
-    {
+    };
+    this.uploader.onSuccessItem = (item, response, status, headers) => {
       const photo = JSON.parse(response);
-      const updatedMember = {...this.member()}
+      const updatedMember = { ...this.member() };
       updatedMember.photos.push(photo);
       this.memberChange.emit(updatedMember);
-    }
+    };
   }
 }
